f5fbd3b0d2eb9f96dbd9d3a34fd54ab9fca684d3
Gaëtan <59415807+gaetanbrison@users.noreply.github.com>
2025-06-12 16:06:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ import streamlit as st
 import numpy as np
 import sklearn
 
-from ydata_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(
     page_title="Califronia Housing Dashboard 🏡",
